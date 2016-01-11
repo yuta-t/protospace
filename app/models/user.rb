99ed6932a1d7e77e5aec129c2d0c85b26bdea617
yuta-t <yutatuy.t@gmail.com>
2016-01-11 20:05:09 +0900

    Add validation to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates_presence_of [:user_name, :image, :profile, :occupation, :position]
   mount_uploader :image, ImageUploader
+
+  has_many :prototypes
 end
