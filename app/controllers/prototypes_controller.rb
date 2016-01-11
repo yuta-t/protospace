487178507616fa8435135896f6d305502a32151e
yuta-t <yutatuy.t@gmail.com>
2016-01-11 18:43:51 +0900

    Add routing and actions

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,18 @@
 class PrototypesController < ApplicationController
   def index
   end
+
+  def new
+  end
+
+  def create
+    redirect_to :index
+  end
+
+  def show
+  end
+
+  private
+  def create_params
+  end
 end
