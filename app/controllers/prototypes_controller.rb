bd5adbfe90c447da5e31e758d1f1dad2bb9bf9e6
yuta-t <yutatuy.t@gmail.com>
2016-01-13 16:02:23 +0900

    Add routings and action about prototypes#edit, update and delete

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,13 @@
 class PrototypesController < ApplicationController
+  before_action :get_prototype, only: [:show, :edit, :update, :delete]
+
   def index
     @prototypes = Prototype.page(params[:page])
+  end
+
+  def show
+    @user = @prototype.user
+    @sub_images = @prototype.images.sub
   end
 
   def new
@@ -13,13 +20,20 @@ class PrototypesController < ApplicationController
     redirect_to action: :index
   end
 
-  def show
-    @prototype = Prototype.find(params[:id])
-    @user = @prototype.user
-    @sub_images = @prototype.images.sub
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
   end
 
   private
+  def get_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
   def create_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, images_attributes: [:name, :image_type])
   end
