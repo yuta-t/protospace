93da2a9c5d024fd634bd9c97a882e00c8700b96f
yuta-t <yutatuy.t@gmail.com>
2016-01-18 20:31:09 +0900

    Change action and create destroy action

--- app/controllers/prototypes/comments_controller.rb
+++ app/controllers/prototypes/comments_controller.rb
@@ -1,11 +1,22 @@
 class Prototypes::CommentsController < ApplicationController
+  before_action :get_prototype, only: [:create, :destroy]
+
   def create
-    @prototype = Prototype.find(params[:prototype_id])
     @prototype.comments.create(comment_params)
-    @comments = @prototype.comments
+    @comments = @prototype.comments.without_soft_destroyed
+  end
+
+  def destroy
+    comment = Comment.find(params[:id])
+    comment.soft_destroy
+    @comments = @prototype.comments.without_soft_destroyed
   end
 
   private
+  def get_prototype
+    @prototype = Prototype.find(params[:prototype_id])
+  end
+
   def comment_params
     params.require(:comment).permit(:comment)
           .merge(user_id: current_user.id)
