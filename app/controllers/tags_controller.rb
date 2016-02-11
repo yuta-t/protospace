88c07cc2f7837ce53c75db1cb10c79bd7c5949ac
yuta-t <yutatuy.t@gmail.com>
2016-02-11 19:50:18 +0900

    create view tag#show

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,8 +1,22 @@
 class TagsController < ApplicationController
+  before_action :set_type
+
   def index
-    @type = 'tags'
+    @tags = ActsAsTaggableOn::Tag.most_used
   end
 
   def show
+    @tag = params[:tag_name]
+    @prototypes = Prototype.tagged_with(@tag)
+                           .without_soft_destroyed
+                           .includes(:user, :main_image)
+                           .order({likes_count: :DESC,
+                                    created_at: :DESC})
+                           .page(params[:page])
+  end
+
+  private
+  def set_type
+    @type = 'tags'
   end
 end
